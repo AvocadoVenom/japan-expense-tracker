7e257451ef0f597331df8a70753997a6cc8e08d7
QiMirda <admiquel94@gmail.com>
2025-09-12 12:51:57 +0900

    CSS cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
 import FloatingMenuButton from "./FloatingMenuButton";
 
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Japan Expense Tracker",
@@ -27,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="w-screen h-screen p-10">
         {children}
         <FloatingMenuButton />
       </body>
