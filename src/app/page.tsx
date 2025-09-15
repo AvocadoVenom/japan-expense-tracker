49f0aa5cf1a13553489c1376cc7dd6d8242f535c
QiMirda <admiquel94@gmail.com>
2025-09-15 17:52:25 +0900

    Revert "Remove Toast and refetch function"
    This reverts commit 9ac7fcc1e7b281c217d6c38da8563c509b1a8b6e.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useState } from "react";
 import { useExpenseRules } from "./api/hooks/expense-rules";
 import { useAddExpense, useExpenses } from "./api/hooks/expenses";
 import { ExpenseForm, ExpenseFormData } from "./components/ExpenseForm";
 import { ExpensesSummary } from "./components/ExpensesSummary";
+import { Toast } from "@/app/components/atoms/Toast";
 
 export default function Home() {
   const { data: todayExpenses, status: expensesFetchingStatus } = useExpenses({
@@ -11,6 +13,7 @@ export default function Home() {
   });
   const { data: rules, status: rulesFetchingStatus } = useExpenseRules();
   const { mutate: addExpense } = useAddExpense();
+  const [toast, setToast] = useState<string | null>(null);
 
   if (
     expensesFetchingStatus === "success" &&
@@ -21,9 +24,11 @@ export default function Home() {
         <ExpenseForm
           onExpenseSubmitted={async (formData: ExpenseFormData) => {
             await addExpense(formData);
+            setToast("Expense saved ✅");
           }}
         />
         <ExpensesSummary expenses={todayExpenses} rules={rules} />
+        {toast && <Toast message={toast} onClose={() => setToast(null)} />}
       </main>
     );
   }
